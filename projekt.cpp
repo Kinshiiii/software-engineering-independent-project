74f615394d1a0bf06fab31752b360bade1a88f70
Kinshiiii <samueljuliankepski@gmail.com>
2025-11-02 17:08:25 +0100

    Dodano sprawdzanie dodatności liczby

--- projekt.cpp
+++ projekt.cpp
@@ -6,6 +6,11 @@ void wykonaj_operacje() {
     int liczba;
     cout << "Podaj liczbę całkowitą dodatnią: ";
     cin >> liczba;
+
+    if (liczba <= 0) {
+        cout << "Podano liczbę niedodatnią. Program zakończy działanie." << endl;
+        return;
+    }
     
     cout << "Wpisałeś liczbę: " << liczba << endl;
 }
