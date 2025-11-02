cf636d991036da6365b0bed81424a1b2ccfeee87
Kinshiiii <samueljuliankepski@gmail.com>
2025-11-02 17:05:45 +0100

    Dodano funkcję czy_pierwsza i informację o liczbie pierwszej

--- projekt.cpp
+++ projekt.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+bool czy_pierwsza(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 void wykonaj_operacje() {
     int liczba;
     cout << "Podaj liczbę całkowitą dodatnią: ";
     cin >> liczba;
-    
-    cout << "Wpisałeś liczbę: " << liczba << endl;
+
+    cout << "Wpisałeś liczbę: " << liczba << (czy_pierwsza(liczba) ? " (liczba pierwsza)" : " (liczba złożona)") << endl;
 }
 
 int main() {
